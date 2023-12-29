9828ac7f6c0ef800f16441f4563cad88905a8860
Szymon Zieliński <szielinski1508@wp.pl>
2023-12-29 16:42:46 +0100

    Initial Commit

---
+++ src/test/java/com/example/praza_inzynierska/PrazaInzynierskaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praza_inzynierska;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrazaInzynierskaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
