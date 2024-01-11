8ad2b15a174f19982fa6ac65c67e69b32432483e
Szymon Zieliński <szielinski1508@wp.pl>
2024-01-11 22:16:18 +0100

    Feature; Reformat code

--- src/main/java/com/example/praza_inzynierska/PrazaInzynierskaApplication.java
+++ src/main/java/com/example/praza_inzynierska/PrazaInzynierskaApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class PrazaInzynierskaApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(PrazaInzynierskaApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(PrazaInzynierskaApplication.class, args);
+    }
 
 }
--- src/main/java/com/example/praza_inzynierska/services/UserService.java
+++ src/main/java/com/example/praza_inzynierska/services/UserService.java
@@ -8,9 +8,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
